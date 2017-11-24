67e579732d323e66b3deec52d0f10bbeb15096f9
SpadeEm <965702794@qq.com>
2017-11-24 14:16:13 +0800

    去掉无用的注释

--- src/com/java/dao/UserDao.java
+++ src/com/java/dao/UserDao.java
@@ -1,11 +1,8 @@
 package com.java.dao;
 
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
-
 import com.java.entity.User;
 
-@Repository
 public interface UserDao {
 	
 	public User login(@Param("userName")String userName,@Param("userPassword")String userPassword);
